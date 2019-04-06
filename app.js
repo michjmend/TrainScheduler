2e20bd4cf29297b71de5a7c299f6683817669e7d
Peanut <michjmend@gmail.com>
2019-04-06 19:41:25 -0400

    anotha one

---
+++ app.js
@@ -0,0 +1,54 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCuW4am7X3sGw_mJTRx3k_U17ol2GvIvdw",
+    authDomain: "train-project-b601f.firebaseapp.com",
+    databaseURL: "https://train-project-b601f.firebaseio.com",
+    projectId: "train-project-b601f",
+    storageBucket: "train-project-b601f.appspot.com",
+    messagingSenderId: "823317561192"
+  };
+
+  firebase.initializeApp(config);
+
+  var database = firebase.database()
+
+  $("#submit").on("click", function(event){
+    event.preventDefault()
+    var name = $("#train-name").val().trim()
+    var destination = $("#destination").val().trim()
+    var firstTrainTime = $("#first-train-time").val().trim()
+    var frequency = $("#frequency").val().trim()
+
+    firstTrainTime = moment(firstTrainTime, "HH:mm").subtract(1, "years");
+
+    var diffTime = moment().diff(moment(firstTrainTime), "minutes");
+
+    var tRemainder = diffTime % frequency;
+
+    var minutesAway = frequency - tRemainder;
+
+    var nextArrival = moment().add(minutesAway, "minutes");
+
+    database.ref().push({
+      name : name,
+      destination : destination,
+      firstTrainTime : firstTrainTime,
+      frequency : frequency,
+    })
+
+    $("#train-name").val("");
+    $("#destination").val("");
+    $("#first-train-time").val("");
+    $("#frequency").val("")
+  })
+
+  database.ref().on("child_added", function(childSnapshot) {
+    var newLine = childSnapshot.val();
+    console.log(newLine);
+    $("#table-area").append("<tr>")
+    $("#table-area").append("<td>" + newLine.name)
+    $("#table-area").append("<td>" + newLine.destination)
+    $("#table-area").append("<td>" + newLine.frequency)
+    $("#table-area").append("<td>" + newLine.nextArrival)
+    $("#table-area").append("<td>" + newLine.minutesAway)
+  });
