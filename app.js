fb703131e41e50709b33ce8e30bbe43d0d84aa0f
Peanut <michjmend@gmail.com>
2019-04-06 19:49:57 -0400

    idk

--- app.js
+++ app.js
@@ -30,14 +30,14 @@
     var nextArrival = moment().add(minutesAway, "minutes");
 
 // I am unable to get the minutes away and next arrival fixed :(
-  //also unable to get things t push up
-  //cant figure out why the console log says that firstTrainTime is not defined when it definitely is!!
 
     database.ref().push({
       name : name,
       destination : destination,
       // firstTrainTime : firstTrainTime,
       frequency : frequency,
+      nextArrival : nextArrival,
+      minutesAway : minutesAway,
     })
 
     $("#train-name").val("");
